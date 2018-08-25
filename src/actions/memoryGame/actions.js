86084ae15b90afdea096fe77a44a22d3468087f5
amirplanet <amir.waisman@planet.com>
2018-08-25 21:09:27 +0200

    Added style, confetti, and new game button

--- src/actions/memoryGame/actions.js
+++ src/actions/memoryGame/actions.js
@@ -1,47 +1,42 @@
-import { SET_SELECTED, DESELECT_ALL, RESTART_GAME, WIN_GAME, PICK_LETTERS, SET_MATCHED, TOGGLE_BLOCK_SELECTION } from './types';
+import { SET_SELECTED, DESELECT_ALL, RESTART_GAME, PICK_LETTERS, SET_MATCHED, TOGGLE_BLOCK_SELECTION } from './types';
 
-export const selectChar = (id, char) => dispatch => {
-  dispatch({
-    type: SET_SELECTED,
-    id,
-    char
-  });
+export const selectChar = (id, char) => (dispatch) => {
+	dispatch({
+		type: SET_SELECTED,
+		id,
+		char
+	});
 };
 
-export const setMatched = (char) => dispatch => {
-  dispatch({
-    type: SET_MATCHED,
-    payload: char
-  });
+export const setMatched = (char) => (dispatch) => {
+	dispatch({
+		type: SET_MATCHED,
+		payload: char
+	});
 };
 
-export const deselectAll = () => dispatch => {
-  dispatch({
-    type: DESELECT_ALL,
-  });
+export const deselectAll = () => (dispatch) => {
+	dispatch({
+		type: DESELECT_ALL
+	});
 };
 
-export const restartGame = () => dispatch => {
-  dispatch({
-    type: RESTART_GAME
-  });
+export const restartGame = (newLetters) => (dispatch) => {
+	dispatch({
+		type: RESTART_GAME,
+		payload: newLetters
+	});
 };
 
-export const winGame = () => dispatch => {
-  dispatch({
-    type: WIN_GAME
-  });
+export const pickLetters = (letters) => (dispatch) => {
+	dispatch({
+		type: PICK_LETTERS,
+		payload: letters
+	});
 };
 
-export const pickLetters = (letters) => dispatch => {
-  dispatch({
-    type: PICK_LETTERS,
-    payload: letters
-  });
-};
-
-export const toggleBlockSelection = () => dispatch => {
-  dispatch({
-    type: TOGGLE_BLOCK_SELECTION
-  });
+export const toggleBlockSelection = () => (dispatch) => {
+	dispatch({
+		type: TOGGLE_BLOCK_SELECTION
+	});
 };
